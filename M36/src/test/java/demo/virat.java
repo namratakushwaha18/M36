5c16e0d07ce9d1091c1fea826e035f35880011d2
ajay1 <ajay1@AASHVI>
2025-09-26 07:52:50 +0530

    COMMIT VIRAT

---
+++ M36/src/test/java/demo/virat.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class virat {
+
+	public static void main(String[] args) {
+		int a = 20;
+        int b = 8;
+        int result = a - b;
+
+        System.out.println("Subtraction: " + result);
+
+	}
+
+}
